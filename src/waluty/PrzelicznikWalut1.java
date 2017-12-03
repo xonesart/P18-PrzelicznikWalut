648828159540cf3d92e3f6652a2d668905f64977
Patryk Czarnik <mail@patrykczarnik.net>
2017-12-03 12:34:27 +0100

    Pierwsza wersja GUI - tylko makieta

---
+++ src/waluty/PrzelicznikWalut1.java
@@ -0,0 +1,179 @@
+package waluty;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import java.awt.Font;
+import javax.swing.JComboBox;
+import javax.swing.DefaultComboBoxModel;
+
+public class PrzelicznikWalut1 {
+
+	private static final Font FONT_LABEL = new Font("Dialog", Font.PLAIN, 24);
+	private static final Font FONT_DANE = new Font("Dialog", Font.BOLD, 24);
+	private JFrame frmPrzelicznikWalut;
+	private JLabel lblNrTabeli;
+	private JLabel lblDataTabeli;
+	private JComboBox<String> comboBoxWaluta;
+	private JLabel lblNazwaWaluty;
+	private JLabel lblKurs_1;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					PrzelicznikWalut1 window = new PrzelicznikWalut1();
+					window.frmPrzelicznikWalut.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public PrzelicznikWalut1() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frmPrzelicznikWalut = new JFrame();
+		frmPrzelicznikWalut.setTitle("Przelicznik Walut");
+		frmPrzelicznikWalut.setBounds(100, 100, 742, 593);
+		frmPrzelicznikWalut.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JPanel panel = new JPanel();
+		
+		JPanel panel_1 = new JPanel();
+		GroupLayout groupLayout = new GroupLayout(frmPrzelicznikWalut.getContentPane());
+		groupLayout.setHorizontalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(Alignment.TRAILING, groupLayout.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
+						.addComponent(panel_1, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 710, Short.MAX_VALUE)
+						.addComponent(panel, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 710, Short.MAX_VALUE))
+					.addContainerGap())
+		);
+		groupLayout.setVerticalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(panel, GroupLayout.PREFERRED_SIZE, 113, GroupLayout.PREFERRED_SIZE)
+					.addGap(18)
+					.addComponent(panel_1, GroupLayout.PREFERRED_SIZE, 146, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(271, Short.MAX_VALUE))
+		);
+		
+		JLabel lblWybierzWalut = new JLabel("Wybierz walutę");
+		lblWybierzWalut.setFont(new Font("Dialog", Font.PLAIN, 24));
+		
+		comboBoxWaluta = new JComboBox<>();
+		comboBoxWaluta.setModel(new DefaultComboBoxModel<>(new String[] {"EUR", "USD", "CHF"}));
+		comboBoxWaluta.setFont(FONT_DANE);
+		
+		JLabel lblWaluta = new JLabel("Waluta");
+		lblWaluta.setFont(new Font("Dialog", Font.PLAIN, 24));
+		
+		JLabel lblKurs = new JLabel("Kurs");
+		lblKurs.setFont(new Font("Dialog", Font.PLAIN, 24));
+		
+		lblNazwaWaluty = new JLabel("nrTabeli");
+		lblNazwaWaluty.setFont(new Font("Dialog", Font.BOLD, 24));
+		
+		lblKurs_1 = new JLabel("nrTabeli");
+		lblKurs_1.setFont(new Font("Dialog", Font.BOLD, 24));
+		GroupLayout gl_panel_1 = new GroupLayout(panel_1);
+		gl_panel_1.setHorizontalGroup(
+			gl_panel_1.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel_1.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(gl_panel_1.createParallelGroup(Alignment.LEADING)
+						.addComponent(lblWybierzWalut, GroupLayout.PREFERRED_SIZE, 197, GroupLayout.PREFERRED_SIZE)
+						.addComponent(lblWaluta, GroupLayout.PREFERRED_SIZE, 150, GroupLayout.PREFERRED_SIZE)
+						.addComponent(lblKurs, GroupLayout.PREFERRED_SIZE, 150, GroupLayout.PREFERRED_SIZE))
+					.addGap(43)
+					.addGroup(gl_panel_1.createParallelGroup(Alignment.LEADING)
+						.addComponent(lblKurs_1, GroupLayout.PREFERRED_SIZE, 147, GroupLayout.PREFERRED_SIZE)
+						.addGroup(gl_panel_1.createParallelGroup(Alignment.LEADING, false)
+							.addComponent(lblNazwaWaluty, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+							.addComponent(comboBoxWaluta, 0, 147, Short.MAX_VALUE)))
+					.addContainerGap(311, Short.MAX_VALUE))
+		);
+		gl_panel_1.setVerticalGroup(
+			gl_panel_1.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel_1.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(gl_panel_1.createParallelGroup(Alignment.LEADING)
+						.addComponent(comboBoxWaluta, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(lblWybierzWalut, GroupLayout.PREFERRED_SIZE, 29, GroupLayout.PREFERRED_SIZE))
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addGroup(gl_panel_1.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_panel_1.createSequentialGroup()
+							.addComponent(lblWaluta, GroupLayout.PREFERRED_SIZE, 29, GroupLayout.PREFERRED_SIZE)
+							.addPreferredGap(ComponentPlacement.UNRELATED)
+							.addComponent(lblKurs, GroupLayout.PREFERRED_SIZE, 29, GroupLayout.PREFERRED_SIZE))
+						.addGroup(gl_panel_1.createSequentialGroup()
+							.addComponent(lblNazwaWaluty, GroupLayout.PREFERRED_SIZE, 29, GroupLayout.PREFERRED_SIZE)
+							.addGap(18)
+							.addComponent(lblKurs_1, GroupLayout.PREFERRED_SIZE, 29, GroupLayout.PREFERRED_SIZE)))
+					.addContainerGap(38, Short.MAX_VALUE))
+		);
+		panel_1.setLayout(gl_panel_1);
+		
+		JLabel lblNumerTabeli = new JLabel("Numer tabeli");
+		lblNumerTabeli.setFont(FONT_LABEL);
+		
+		JLabel lblData = new JLabel("Data");
+		lblData.setFont(FONT_LABEL);
+		
+		lblNrTabeli = new JLabel("nrTabeli");
+		lblNrTabeli.setFont(FONT_DANE);
+		
+		lblDataTabeli = new JLabel("dataTabeli");
+		lblDataTabeli.setFont(FONT_DANE);
+		
+		GroupLayout gl_panel = new GroupLayout(panel);
+		gl_panel.setHorizontalGroup(
+			gl_panel.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(gl_panel.createParallelGroup(Alignment.TRAILING, false)
+						.addComponent(lblData, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+						.addComponent(lblNumerTabeli, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 150, Short.MAX_VALUE))
+					.addGap(92)
+					.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
+						.addComponent(lblDataTabeli, GroupLayout.PREFERRED_SIZE, 149, GroupLayout.PREFERRED_SIZE)
+						.addComponent(lblNrTabeli, GroupLayout.PREFERRED_SIZE, 138, GroupLayout.PREFERRED_SIZE))
+					.addContainerGap(307, Short.MAX_VALUE))
+		);
+		gl_panel.setVerticalGroup(
+			gl_panel.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(gl_panel.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblNumerTabeli)
+						.addComponent(lblNrTabeli))
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addGroup(gl_panel.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblData)
+						.addComponent(lblDataTabeli))
+					.addContainerGap(31, Short.MAX_VALUE))
+		);
+		panel.setLayout(gl_panel);
+		frmPrzelicznikWalut.getContentPane().setLayout(groupLayout);
+	}
+}
