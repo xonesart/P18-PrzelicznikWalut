ea84edd743869bad826ffd594c5b79137d6d0d40
Patryk Czarnik <mail@patrykczarnik.net>
2017-12-03 12:57:33 +0100

    Wybór waluty z listy

--- src/waluty/PrzelicznikWalut1.java
+++ src/waluty/PrzelicznikWalut1.java
@@ -14,6 +14,8 @@ import java.util.concurrent.ExecutionException;
 
 import javax.swing.JComboBox;
 import javax.swing.DefaultComboBoxModel;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class PrzelicznikWalut1 {
 
@@ -26,6 +28,7 @@ public class PrzelicznikWalut1 {
 	private JLabel lblNazwaWaluty;
 	private JLabel lblKurs_1;
 	private Tabela tabela;
+	private Waluta wybranaWaluta;
 
 	/**
 	 * Launch the application.
@@ -87,6 +90,11 @@ public class PrzelicznikWalut1 {
 		lblWybierzWalut.setFont(new Font("Dialog", Font.PLAIN, 24));
 		
 		comboBoxWaluta = new JComboBox<>();
+		comboBoxWaluta.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent ev) {
+				zmianaWaluty();
+			}
+		});
 		comboBoxWaluta.setModel(new DefaultComboBoxModel<>(new String[] {"EUR", "USD", "CHF"}));
 		comboBoxWaluta.setFont(FONT_DANE);
 		
@@ -96,10 +104,10 @@ public class PrzelicznikWalut1 {
 		JLabel lblKurs2 = new JLabel("Kurs");
 		lblKurs2.setFont(new Font("Dialog", Font.PLAIN, 24));
 		
-		lblNazwaWaluty = new JLabel("nrTabeli");
+		lblNazwaWaluty = new JLabel("--");
 		lblNazwaWaluty.setFont(new Font("Dialog", Font.BOLD, 24));
 		
-		lblKurs_1 = new JLabel("nrTabeli");
+		lblKurs_1 = new JLabel("--");
 		lblKurs_1.setFont(new Font("Dialog", Font.BOLD, 24));
 		GroupLayout gl_panel_1 = new GroupLayout(panel_1);
 		gl_panel_1.setHorizontalGroup(
@@ -112,19 +120,18 @@ public class PrzelicznikWalut1 {
 						.addComponent(lblKurs2, GroupLayout.PREFERRED_SIZE, 150, GroupLayout.PREFERRED_SIZE))
 					.addGap(43)
 					.addGroup(gl_panel_1.createParallelGroup(Alignment.LEADING)
-						.addComponent(lblKurs_1, GroupLayout.PREFERRED_SIZE, 147, GroupLayout.PREFERRED_SIZE)
-						.addGroup(gl_panel_1.createParallelGroup(Alignment.LEADING, false)
-							.addComponent(lblNazwaWaluty, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-							.addComponent(comboBoxWaluta, 0, 147, Short.MAX_VALUE)))
-					.addContainerGap(311, Short.MAX_VALUE))
+						.addComponent(comboBoxWaluta, 0, 446, Short.MAX_VALUE)
+						.addComponent(lblNazwaWaluty, GroupLayout.DEFAULT_SIZE, 446, Short.MAX_VALUE)
+						.addComponent(lblKurs_1, GroupLayout.DEFAULT_SIZE, 446, Short.MAX_VALUE))
+					.addContainerGap())
 		);
 		gl_panel_1.setVerticalGroup(
 			gl_panel_1.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_panel_1.createSequentialGroup()
 					.addContainerGap()
 					.addGroup(gl_panel_1.createParallelGroup(Alignment.LEADING)
-						.addComponent(comboBoxWaluta, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-						.addComponent(lblWybierzWalut, GroupLayout.PREFERRED_SIZE, 29, GroupLayout.PREFERRED_SIZE))
+						.addComponent(lblWybierzWalut, GroupLayout.PREFERRED_SIZE, 29, GroupLayout.PREFERRED_SIZE)
+						.addComponent(comboBoxWaluta, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
 					.addPreferredGap(ComponentPlacement.UNRELATED)
 					.addGroup(gl_panel_1.createParallelGroup(Alignment.LEADING)
 						.addGroup(gl_panel_1.createSequentialGroup()
@@ -135,7 +142,7 @@ public class PrzelicznikWalut1 {
 							.addComponent(lblNazwaWaluty, GroupLayout.PREFERRED_SIZE, 29, GroupLayout.PREFERRED_SIZE)
 							.addGap(18)
 							.addComponent(lblKurs_1, GroupLayout.PREFERRED_SIZE, 29, GroupLayout.PREFERRED_SIZE)))
-					.addContainerGap(38, Short.MAX_VALUE))
+					.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
 		);
 		panel_1.setLayout(gl_panel_1);
 		
@@ -145,10 +152,10 @@ public class PrzelicznikWalut1 {
 		JLabel lblData = new JLabel("Data");
 		lblData.setFont(FONT_LABEL);
 		
-		lblNrTabeli = new JLabel("nrTabeli");
+		lblNrTabeli = new JLabel("--");
 		lblNrTabeli.setFont(FONT_DANE);
 		
-		lblDataTabeli = new JLabel("dataTabeli");
+		lblDataTabeli = new JLabel("--");
 		lblDataTabeli.setFont(FONT_DANE);
 		
 		GroupLayout gl_panel = new GroupLayout(panel);
@@ -203,7 +210,7 @@ public class PrzelicznikWalut1 {
 					
 					// Uaktualniam to, co wyświetla okno
 					uaktualnijWidokTabeli();
-					uaktualnijWaluty();
+					zmianaWaluty();
 					
 				} catch (InterruptedException | ExecutionException e) {
 				}
@@ -219,9 +226,20 @@ public class PrzelicznikWalut1 {
 		lblDataTabeli.setText(tabela.getData().toString());
 	}
 
-	protected void uaktualnijWaluty() {
-		// TODO Auto-generated method stub
+	protected void uaktualnijWidokWaluty() {
+		if(wybranaWaluta != null) {
+			lblNazwaWaluty.setText(wybranaWaluta.getNazwa());
+			lblKurs_1.setText(wybranaWaluta.getKurs().toString());
+		}
 		
+	}
+
+	protected void zmianaWaluty() {
+		String kod = (String)comboBoxWaluta.getSelectedItem();
+		if(tabela != null) {
+			wybranaWaluta = tabela.znajdz(kod);
+			uaktualnijWidokWaluty();
+		}
 	}
 
 }
